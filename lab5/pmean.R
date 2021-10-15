9dea112e98045846b152e64b8e5b50047a3548a0
Eugene <zerom2016romanenko@gmail.com>
2021-10-15 19:01:15 +0300

    add complete function, fix pmean function

--- lab5/pmean.R
+++ lab5/pmean.R
@@ -1,31 +1,45 @@
-pmean <- function(directory, pollutant, id=1:332){
-  common_mean  <- 0
+read_file <- function(directory, file_id){
+  file_name <- paste(file_id)
   
-  for(file_id in id){
-    file_name <- paste(file_id)
-    
-    while(nchar(file_name) < 3){
-      file_name <- paste(0, file_name, sep = "")
-    }
-    file_name <- paste(file_name, ".csv", sep = "")
-    
-    file_path <- file.path(directory, file_name, fsep = .Platform$file.sep)
-    
-    read_data <- read.csv(file_path)
-    
-    if(length(read_data[[pollutant]][!is.na(read_data[[pollutant]])]) == 0){
-      current_mean <- 0
-    }
-    else{
-      current_mean <-  mean(read_data[[pollutant]][!is.na(read_data[[pollutant]])])
-    }
-    
-    common_mean <- common_mean + current_mean
+  while(nchar(file_name) < 3){
+    file_name <- paste(0, file_name, sep = "")
   }
+  file_name <- paste(file_name, ".csv", sep = "")
   
-  return(common_mean / length(id))
+  file_path <- file.path(directory, file_name, fsep = .Platform$file.sep)
+  
+  read_data <- read.csv(file_path)
+  
+  return(read_data)
 }
 
-test <- pmean("specdata", "t", 1)
-print(test)
+pmean <- function(directory, pollutant, id=1:332){
+  combine_vector <- vector()
+  
+  for(file_id in id){
+    
+    read_data <- read_file(directory, file_id)
+    
+    combine_vector <- c(combine_vector, read_data[[pollutant]][!is.na(read_data[[pollutant]])])
+  }
+  
+  return(mean(combine_vector))
+}
+
+complete <- function(directory, id){
+  result <- data.frame(matrix(ncol = 2, nrow = 0))
+  
+  for(file_id in id){
+    read_data <- read_file(directory, file_id)
+    
+    new_row <- c(file_id, nrow(read_data[!is.na(read_data$Date) & !is.na(read_data$sulfate) & !is.na(read_data$nitrate),]))
+    
+    result <- rbind(result, new_row)
+  }
+  
+  col_names <- c("file_id", "number_of_completely_observed_cases")
+  colnames(result) <- col_names
+  
+  return(result)
+}
 
